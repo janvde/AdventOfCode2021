ebebfa3efef8abce1c27c37b1a6e9fba65dc796a
Jan van den Ende <jan.vandenende@emburse.com>
2021-12-05 21:49:23 +0100

    day 5 part 2

--- src/main/kotlin/day5/part2/Part2.kt
+++ src/main/kotlin/day5/part2/Part2.kt
@@ -1,11 +1,9 @@
-package day5.part1
+package day5.part2
 
 import java.io.File
 
 fun main(args: Array<String>) {
-    val input = readInput().filter {
-        it.start.x == it.end.x || it.start.y == it.end.y
-    }
+    val input = readInput()
 
     println(countOverlaps(input))
 }
@@ -22,15 +20,20 @@ fun Line.toPoints(): List<Point> {
     val horizontalRange = if (start.x <= end.x) start.x..end.x else start.x downTo end.x
     val verticalRange = if (start.y <= end.y) start.y..end.y else start.y downTo end.y
 
-    return if (horizontalRange.toList().size > verticalRange.toList().size) {
+    val points =  if (horizontalRange.toList().size > verticalRange.toList().size) {
         horizontalRange.toList().map {
             Point(it, end.y)
         }
-    } else {
+    } else if(horizontalRange.toList().size < verticalRange.toList().size) {
         verticalRange.toList().map {
             Point(start.x, it)
         }
+    } else {
+        verticalRange.toList().mapIndexed { index, i ->
+            Point(horizontalRange.toList().get(index), i)
+        }
     }
+    return points
 }
 
 fun countOverlaps(lines: List<Line>): Int {
